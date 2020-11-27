e3f21abe2351c7852ca027b7f624c0e79423da9f
Ryan John Vickramasinghe <rvickram@uwo.ca>
2020-11-26 20:58:32 -0500

    Display messages on the dashboard.

--- src/context/ConverstationsProvider.js
+++ src/context/ConverstationsProvider.js
@@ -8,7 +8,7 @@ export function useConversations() {
   return useContext(ConversationsContext)
 }
 
-export function ConversationsProvider({ children }) {
+export function ConversationsProvider({ children, id }) {
   const [conversations, setConversations] = useLocalStorage('conversations', [])
   const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
   const { contacts } = useContacts()
@@ -17,6 +17,39 @@ export function ConversationsProvider({ children }) {
     setConversations(prevConversations => {
       return [...prevConversations, { recipients, messages: [] }]
     })
+  }
+
+  function addMessageToConversation({ recipients, text, sender}) {
+    setConversations(prevConversations => {
+      let madeChange = false
+      const newMessage = { sender, text }
+      const newConversations = prevConversations.map(conversation => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          // if the conversation already exists, add a message to it
+
+          madeChange = true
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage]
+          }
+        }
+
+        // otherwise, return the same convo
+        return conversation
+      })
+
+      if (madeChange) {
+        // found a convo, add message
+        return newConversations
+      } else {
+        // create a new convo
+        return [...prevConversations, { recipients, messages: [newMessage]}]
+      }
+    })
+  }
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id})
   }
 
   // format messages
@@ -29,13 +62,26 @@ export function ConversationsProvider({ children }) {
           const name = (contact && contact.name) || recipient
           return { id: recipient, name }
       })
+
+      const messages = conversation.messages.map(message => {
+        const contact = contacts.find(contact => {
+          return contact.id === message.sender
+        })
+        const name = (contact && contact.name) || message.sender
+
+        const fromMe = id === message.sender
+
+        return { ...message, senderName: name, fromMe}
+      })
+
       const selected = index === selectedConversationIndex
-      return { ...conversation, recipients, selected }
+      return { ...conversation, messages, recipients, selected }
   })
 
   const value = {
     conversations: formattedConversations,
     selectedConversation: formattedConversations[selectedConversationIndex],
+    sendMessage,
     selectedConversationIndex: setSelectedConversationIndex,
     createConversation
   }
@@ -45,4 +91,15 @@ export function ConversationsProvider({ children }) {
       {children}
     </ConversationsContext.Provider>
   )
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false
+
+  a.sort()
+  b.sort()
+
+  return a.every((elmt, index) => {
+    return elmt === b[index]
+  })
 }
